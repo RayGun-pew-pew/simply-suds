a242155b8c996fb6e3542bc9bb8899981c91c9df
Tate Ray <tate@tateray.com>
2024-07-24 11:27:52 -0600

    orders and products successfully being passed to the dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   def index
     if session[:user_id]
       @user = User.find_by({ id: session[:user_id] })
+      @orders = Order.where(purchased: true)
+      @products = Product.all
     else
       redirect_to "/sign_in"
     end
